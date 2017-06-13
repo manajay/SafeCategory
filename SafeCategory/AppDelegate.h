7162c9afe85c05f0b4184ff2fa1b46b11fa69ac0
manajay <lianjie.duan@dingride.com>
2017-06-13 11:24:04 +0800

    Initial Commit

---
+++ SafeCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SafeCategory
+//
+//  Created by manajay on 13/06/2017.
+//  Copyright © 2017 ddtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SafeCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeCategory
+//
+//  Created by manajay on 13/06/2017.
+//  Copyright © 2017 ddtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
